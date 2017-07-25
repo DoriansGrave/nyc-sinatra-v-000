47924d9dbf742efcfa067ff9b5e61f5b34103afb
Scott Jurkowski <scottjurkowski@Crash-Cart.local>
2017-07-25 16:06:07 -0500

    Done.

--- config/environment.rb
+++ config/environment.rb
@@ -4,8 +4,8 @@ Bundler.require
 ENV['SINATRA_ENV'] ||= "development"
 
 ActiveRecord::Base.establish_connection(
-  :adapter => "sqlite3",
-  :database => "db/nyc#{ENV['SINATRA_ENV']}.sqlite"
+    :adapter => "sqlite3",
+    :database => "db/nyc#{ENV['SINATRA_ENV']}.sqlite"
 )
 
 require_relative "../app/controllers/application_controller.rb"
